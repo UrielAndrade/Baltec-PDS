0daf68eed219af1c361955705474cf8b69a03e80
Letícia Soares <leticias.colman@gmail.com>
2026-09-22 13:36:38 -0400

    Catálogo de Componentes, Peças e Controle de Estoque

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+
 using Baltec.Components;
 using Baltec.configs;
 using Baltec.DAO;
@@ -18,6 +19,10 @@ builder.Services.AddScoped<EquipamentoDAO>();
 // DAO para requisições de peças das Ordens de Serviço
 builder.Services.AddScoped<OrdemServicoPecaDAO>();
 builder.Services.AddScoped<OrdemServicoDAO>();
+
+// DAOs para componentes e controle de estoque
+builder.Services.AddScoped<ComponenteDAO>();
+builder.Services.AddScoped<MovimentacaoEstoqueDAO>();
 
 var app = builder.Build();
 
---
+++ models/Componente.cs
@@ -0,0 +1,23 @@
+﻿namespace Baltec.models
+{
+    public class Componente
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; } = "";
+
+        public string CodigoItem { get; set; } = "";
+
+        public int FkCategoria { get; set; }
+
+        public string? CategoriaNome { get; set; }
+
+        public int QuantidadeEstoque { get; set; }
+
+        public decimal PrecoUnitario { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
